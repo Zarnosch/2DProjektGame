043061e9ecd845c11574692cd70431017de29b9e
Zarnosch <janoszwei@yahoo.de>
2014-05-02 22:37:37 +0200

    flackerndes bild
    its not a bugg, its a feature

--- core/src/com/game/project/GameMain.java
+++ core/src/com/game/project/GameMain.java
@@ -2,31 +2,56 @@ package com.game.project;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.game.project.boot.BootMain;
+import com.game.project.menu.MenuMain;
 
 public class GameMain extends Game {
-	SpriteBatch batch;
-	Texture img;
+	//Initialize assetManager
+	public AssetManager assetManager = new AssetManager();
 	
-	AssetManager assetManager = new AssetManager();
+	//Loading States	
+	public void LoadBoot() {
+		BootMain bootMain = new BootMain(assetManager);
+		assetManager.load("images/badlogic.jpg",  Texture.class);
+		while(assetManager.getProgress() != 1.0){
+			assetManager.update();
+		}
+		System.out.println(assetManager.isLoaded("images/badlogic.jpg"));
+		
+		super.setScreen(bootMain);
+//		bootMain.render();
+	}
 	
+	public void LoadMenu() {
+		assetManager.load("images/badlogic.jpg",  Texture.class);
+		super.setScreen(new MenuMain());
+	}
 	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("images/badlogic.jpg");
-		assetManager.load("images/badlogic.jpg", Texture.class);
+	public void LoadPlay() {
+		assetManager.load("images/loading.jpg",  Texture.class);
+	}
+	
+	public void LoadScore() {
+		assetManager.load("images/badlogic.jpg",  Texture.class);
 	}
 
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-		Gdx.input.getX();
+	@Override
+	public void create () {
+		LoadBoot();
+
 	}
+	
+	public void draw(){
+		super.render();
+	}
+
+
+	public void gameStates () {
+	}
+
 }
