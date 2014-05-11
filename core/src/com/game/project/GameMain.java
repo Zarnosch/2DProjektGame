4d1e4ae6a7169959c8db508157a376a87e1041d2
Zarnosch <janoszwei@yahoo.de>
2014-05-11 15:33:05 +0200

    Runnable Loading Screen

--- core/src/com/game/project/GameMain.java
+++ core/src/com/game/project/GameMain.java
@@ -11,65 +11,21 @@ import com.game.project.boot.BootMain;
 import com.game.project.menu.MenuMain;
 
 public class GameMain extends Game {
-<<<<<<< HEAD
-	//Initialize assetManager
-	public AssetManager assetManager = new AssetManager();
-	
-	//Loading States	
-	public void LoadBoot() {
-		BootMain bootMain = new BootMain(assetManager);
-		assetManager.load("images/badlogic.jpg",  Texture.class);
-		while(assetManager.getProgress() != 1.0){
-			assetManager.update();
-		}
-		System.out.println(assetManager.isLoaded("images/badlogic.jpg"));
-		
-		super.setScreen(bootMain);
-		bootMain.render();
-	}
-	
-	public void LoadMenu() {
-		assetManager.load("images/badlogic.jpg",  Texture.class);
-		super.setScreen(new MenuMain());
-	}
-	
-	public void LoadPlay() {
-		assetManager.load("images/loading.jpg",  Texture.class);
-	}
-	
-	public void LoadScore() {
-		assetManager.load("images/badlogic.jpg",  Texture.class);
-	}
-
-	@Override
-	public void create () {
-		LoadBoot();
-
-	}
-	
-	public void draw(){
-		super.render();
-	}
-
-
-	public void gameStates () {
-	}
-=======
     //Initialize assetManager
     public AssetManager assetManager = new AssetManager();
->>>>>>> 67d6a55f2474597650ce3525934fd5c5627f87c8
 
     //Loading States
-/*    public void LoadBoot() {
-        assetManager.load("images/badlogic.jpg",  Texture.class);
+    public void LoadBoot() {
+        assetManager.load("images/loading.jpg",  Texture.class);
         while(assetManager.getProgress() != 1.0){
             assetManager.update();
         }
-        System.out.println(assetManager.isLoaded("images/badlogic.jpg"));
+        System.out.println(assetManager.isLoaded("images/loading.jpg"));
 
         setScreen(new BootMain(assetManager));
+        
     }
-
+    
     public void LoadMenu() {
         assetManager.load("images/badlogic.jpg",  Texture.class);
         super.setScreen(new MenuMain());
@@ -81,16 +37,11 @@ public class GameMain extends Game {
 
     public void LoadScore() {
         assetManager.load("images/badlogic.jpg", Texture.class);
-    }*/
+    }
 
     @Override
     public void create () {
-        assetManager.load("images/badlogic.jpg",  Texture.class);
-        while(assetManager.getProgress() != 1.0){
-            assetManager.update();
-        }
-        System.out.println(assetManager.isLoaded("images/badlogic.jpg"));
-        setScreen(new BootMain(assetManager));
+    	LoadBoot();
     }
 
     public void render(){
--- core/src/com/game/project/boot/BootMain.java
+++ core/src/com/game/project/boot/BootMain.java
@@ -11,29 +11,47 @@ import com.game.project.GameMain;
 
 
 public class BootMain extends ScreenAdapter {
-    private SpriteBatch batch;
-    Texture img;
-    Camera camera;
+	private SpriteBatch batch;
+	Texture img;
+	Camera camera;
 
-    private AssetManager assetManager_;
+	private AssetManager assetManager_;
 
-    public BootMain(AssetManager assMan){
-        assetManager_ = assMan;
+	public BootMain(AssetManager assMan){
+		assetManager_ = assMan;
+		img = assetManager_.get("images/loading.jpg", Texture.class);
 
-        img = assetManager_.get("images/badlogic.jpg", Texture.class);
-    }
+	}
 
-    public void show() {
-        batch = new SpriteBatch();
-    }
+	public void show() {
+		batch = new SpriteBatch();
+	}
 
-    public void render(float delta) {
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+	public void render(float delta) {
+		resize(1920, 1080);
 		Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //assetManager_.update();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		assetManager_.update();
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	@Override
+	public void hide () {
+	}
 
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
-    }
+	@Override
+	public void pause () {
+	}
+
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void dispose () {
+	}
 }
